8602102d54caab1d811f74d2d602c0befeee1c1b
Juano <Juano@172.16.208.82>
2022-11-23 20:46:08 -0500

    Terminado

---
+++ src/main/java/Deber03_01/Principal.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Deber03_01;
+
+/**
+ *
+ * @author Juano
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+
+        //var ventana = new Deber_03_01();
+        //var ventana1 = new Deber_03_01b();
+        ///var ventana2 = new Deber_03_01c();
+        //var ventana3 = new Deber_03_01prueba();
+         var ventana3 = new Deber_03_01d();
+    }
+}
